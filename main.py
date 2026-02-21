a6dccba4e717610d89af22bdf9255ea129246805
Saegl <saegl@protonmail.com>
2026-02-21 15:07:58 +0500

    Init fastapi

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from blog!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def index():
+    return {"message": "Hello from blog 4!"}
